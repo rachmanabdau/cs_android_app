47cd31c40a62602ed564bf57d05376233c0d0083
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2021-01-27 13:47:06 +0700

    Create splash activity

---
+++ app/src/main/java/com/minjem/cs/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.minjem.cs
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        navigateToMainAfterTwoSecond()
+    }
+
+    private fun navigateToMainAfterTwoSecond() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
+    }
+}
