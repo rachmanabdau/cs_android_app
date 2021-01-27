687a17434afe90ec9ff29ac2e3c42994e7f86b46
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2021-01-27 13:41:49 +0700

    Create Network API

---
+++ app/src/main/java/com/minjem/cs/source/network/NetworkAPI.kt
@@ -0,0 +1,26 @@
+package com.minjem.cs.source.network
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+val moshi: Moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl("BASE_URL") // TODO: Replace base url here
+    .build()
+
+interface NetworkService {
+}
+
+object NetworkAPI {
+    val retrofitService: NetworkService by lazy {
+        retrofit.create(NetworkService::class.java)
+    }
+}
